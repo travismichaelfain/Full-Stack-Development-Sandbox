3b4827416173dbc45c0fe623e375718ef79ba77e
travismichaelfain <travismichaelfain@gmail.com>
2026-06-12 12:02:46 -0700

    Delete spacecraft-builder-forms directory

--- spacecraft-builder-forms/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import App from "./App.jsx";
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
